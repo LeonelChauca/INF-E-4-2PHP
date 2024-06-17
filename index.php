6842a639885d0f2795b21f390364c811adc2b7c3
Leonel Chauca <71529078+LeonelChauca@users.noreply.github.com>
2024-06-16 22:30:52 -0400

    terminado

--- index.php
+++ index.php
@@ -1,16 +1,43 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "bdleonel";
-    $conexion = mysqli_connect($servername,$username,$password, $dbname);
-    $consulta = "SELECT * FROM persona";
-    $resultado = mysqli_query($conexion, $consulta);
-    $datos = array();
-
-    while ($fila = mysqli_fetch_assoc($resultado)) {
-        $datos[] = $fila;
-    }
-    mysqli_close($conexion);
-    echo json_encode($datos, JSON_UNESCAPED_UNICODE);
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="mb-4 text-center text-primary">C#</h1>
+        <form action="https://localhost:44369/WebForm1.aspx" method="POST" class="mb-5">
+            <div class="form-group">
+                <label for="nombre">Nombre:</label>
+                <input type="text" class="form-control" id="nombre" name="nombre">
+            </div>
+            <div class="form-group">
+                <label for="edad">Edad:</label>
+                <input type="text" class="form-control" id="apellido" name="apellido">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        
+        <h1 class="mb-4 text-center text-success">Java</h1>
+        <form action="http://localhost:8083/EJER48/NewServlet" method="POST">
+            <div class="form-group">
+                <label for="nombre">Nombre:</label>
+                <input type="text" class="form-control" id="nombre" name="nombre">
+            </div>
+            <div class="form-group">
+                <label for="apellido">Apellido:</label>
+                <input type="text" class="form-control" id="ap" name="ap">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
